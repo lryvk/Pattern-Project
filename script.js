5d8a27a3f6e2f95a213561090807bdda43f418e8
lryv <lukaryvkin@seattleacademy.org>
2024-01-29 12:55:43 -0800

    chage

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const input = document.getElementById(' ');
-const square = document.getElementById(' '); 
-const outputbox1 = document.getElementById(' ')
+var changeBox = {
+    "1": "Box1" ,
+    "2": "Box2" ,
+    "3": "Box3" ,
+    "4": "Box4" ,
+}
+
+const sqaure = document.getElementById('square');
+const input2 = document.getElementById('input2'); 
+
+function changeBox() {
+    const word= textBox1.value;
+    console.log(word)
+    if (word in colorTable) {
+    console.log(colorTable[word])
+        changeBox.style.backgroundColor = changeBox[word];   
+    
+    }
+}
+
+for (let i = 0; i < square.value; i++) {
+    console.log(i)
+  }
+  
+  const textBox1 = document.getElementById('square');
+  textBox1.addEventListener("input", changeBox);
