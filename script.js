3a1f98ac7f1e5e4b6032785095d94d0287a93fbb
lryv <lukaryvkin@seattleacademy.org>
2024-02-02 12:16:25 -0800

    dd

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const rows = document.getElementById ('rows');
 const columns = document.getElementById ('columns');
 const color = document.getElementById ('color');
 const button = document.getElementById ('button');
+const grid = document.getElementById ('grid');
 
 button.addEventListener("click", makeGrid);
 
 function makeGrid() {
 
 grid.innerHTML = "";
+
+    for (let i = 0; i < rows.value; i++) {
+        let rowDiv = document.createElement('div');
+        rowDiv.classicList.add('row');
+        grid.appendChild(rowDiv);
+        for (let j=0; j < columns.value; j++) {
+            let box = document.createElement('div');
+            box.classList.add('box');
+            box.style.backgroundcolor = color.value;
+            rowDiv.appendChild(box);
+        }
+
+    }
 
 }
