75cc1c7deed996d4155cf09c715f4404344ef9cc
lryv <lukaryvkin@seattleacademy.org>
2024-02-06 10:07:54 -0800

    final

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const rows = document.getElementById ('rows');
-const columns = document.getElementById ('columns');
-const color = document.getElementById ('color');
-const button = document.getElementById ('button');
-const grid = document.getElementById ('grid');
+const rows = document.getElementById('rows');
+const columns = document.getElementById('columns');
+const color = document.getElementById('color');
+const submit = document.getElementById('submit');
+const grid = document.getElementById('grid');
 
 submit.addEventListener("click", makeGrid);
 
 function makeGrid() {
-grid.innerHTML = "";
+    grid.innerHTML = "";
     for (let i = 0; i < rows.value; i++) {
         let rowDiv = document.createElement('div');
         rowDiv.classList.add('row');
         grid.appendChild(rowDiv);
-        for (let j=0; j < columns.value; j++) {
+        for (let j = 0; j < columns.value; j++) {
             let box = document.createElement('div');
             box.classList.add('box');
             box.style.backgroundColor = color.value;
+            box.addEventListener('click', changeColor); 
             rowDiv.appendChild(box);
         }
-
     }
+}
 
+function changeColor(event) {
+    let selectedBox = event.target; 
+    selectedBox.style.backgroundColor = color.value; 
 }
